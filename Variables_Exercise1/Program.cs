29eead45fa98f7c362c484a22734800d9cd89c41
martaze blackmon <martazeb47@gmail.com>
2023-06-17 16:39:59 -0400

    Variables Exercise

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Declare and Intitialize variables
+string name = "Martaze";
+int age = 24;
+char middleIntial = 'P';
+bool isOver18 = true;
+double currentTemp = 77.9;
+decimal currentPrice = 33.33m;
+
+//Writing the variables to the console
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Intial: {middleIntial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+
+
+    
